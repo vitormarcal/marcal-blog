86fd3f26b6a597424f097d67cfb0b8ac15959891
Vítor Marçal <codehero@marcal.dev>
2024-02-27 13:28:21 -0300

    add post times

---
+++ components/PostTimes.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps(['createdAt', 'updatedAt'])
+</script>
+
+<template>
+  <p class="post-times">
+              <span>
+              > <time class="created-at">{{ createdAt }}</time>
+            </span>
+
+    <span v-if="createdAt != updatedAt">
+              <i>(Editado em <time class="updated-at">{{ updatedAt }}</time>)</i>
+            </span>
+  </p>
+</template>
+
+<style scoped>
+
+.post-times {
+  font-size: 15px;
+  font-weight: bold;
+}
+
+.updated-at,
+.created-at {
+  margin-top: 2rem;
+  color: hsla(0, 0%, 100%, .64);
+
+  line-height: 1.45;
+  max-width: 840px;
+}
+
+span {
+  margin-right: 5px;
+}
+
+</style>
