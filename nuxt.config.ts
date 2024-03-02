eb0d5f0cc3f40ad01fadcaa11eaa1457fa510558
Vítor Marçal <codehero@marcal.dev>
2024-03-02 13:06:26 -0300

    hotfix Workaround for https://github.com/nuxt/cli/issues/169

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    hooks: {
+        close: () => {
+            // @see https://github.com/nuxt/cli/issues/169#issuecomment-1729300497
+            // Workaround for https://github.com/nuxt/cli/issues/169
+            process.exit(0)
+        }
+    },
     devtools: {enabled: true},
     app: {
       head: {
