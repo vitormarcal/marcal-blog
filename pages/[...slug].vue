6b78aebfa824105e359842791b9de457b9c2f5df
Vítor Marçal <codehero@marcal.dev>
2024-06-29 21:44:07 -0300

    update site

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,37 +1,24 @@
 <template>
   <div>
     <main-content>
-      <comment-area v-if="isPost"/>
+      <template v-if="showCommentArea">
+        <hr/>
+        <email-me-a-comment :email-address="'oi@marcal.dev'" :page-title="page.title" />
+      </template>
+      <hr/>
     </main-content>
   </div>
 
 </template>
 <style>
-.isso-comment.isso-is-page-author > .isso-text-wrapper {
-  background: #1b344d;
-}
 
-.isso-comment > .isso-text-wrapper > .isso-comment-header > .isso-author {
-  color: white;
-}
-
-.isso-comment.isso-is-page-author > .isso-text-wrapper > .isso-comment-header > .isso-author, .isso-page-author-suffix {
-  color: #8DB500;
-}
-
-.isso-auth-section > .isso-post-action > input {
-  background: #8DB500;
-  border-color: #8DB500;
-}
-
-span.marcal_email b {
-  display: none;
-}
 </style>
 <script setup lang="ts">
+import EmailMeAComment from "~/components/EmailMeAComment.vue";
+
 const { page } = useContent()
 
-const isPost = computed(() => {
-  return !!(page?.value?.is_post)
+const showCommentArea = computed(() => {
+  return true
 })
 </script>
