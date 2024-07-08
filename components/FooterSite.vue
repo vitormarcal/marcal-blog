adbc9501ea502559511b8399262542b952cc9b72
Vitor Marçal <codehero@marcal.dev>
2024-07-08 12:23:17 -0300

    Add header (#1)
    * add header
    
    * add footer

---
+++ components/FooterSite.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const thisYear = computed(() => new Date().getFullYear())
+
+</script>
+
+<template>
+  <footer>
+    <hr/>
+    <span>© 2019-{{thisYear}} Feito com muito ❤️ usando Vue e Markdown no Distrito Federal, Brasil  </span>
+    <hr/>
+  </footer>
+
+</template>
+
+<style scoped>
+footer {
+  text-align: center;
+  color: hsla(0, 0%, 100%, .64);
+  font-size: 16px;
+}
+</style>
