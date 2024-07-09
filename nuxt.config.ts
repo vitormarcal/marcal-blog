d2dcb87f34b859f8f1e3ed35c5a10b41f6f81636
Vítor Marçal <codehero@marcal.dev>
2024-07-08 23:58:00 -0300

    Revert "update nuxt and others libs"
    This reverts commit 6981f505b174b2c72bb3d1331fdbabdf6198312d.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,58 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  appConfig: {
-      site: {
-          baseUrl: 'https://www.marcal.dev',
-          title: 'Marçal',
-          description: "Escrevo para eu mesmo como uma forma de documentar e ter um Segundo Cerébro 🧠"
-      },
-      author: {
-          name: 'Vítor Marçal',
-          email: 'oi@marcal.dev'
-      },
-  },
+    appConfig: {
+        site: {
+            baseUrl: 'https://www.marcal.dev',
+            title: 'Marçal',
+            description: "Escrevo para eu mesmo como uma forma de documentar e ter um Segundo Cerébro 🧠"
+        },
+        author: {
+            name: 'Vítor Marçal',
+            email: 'oi@marcal.dev'
+        },
+    },
+    devtools: {enabled: true},
+    app: {
+      head: {
 
-  devtools: {enabled: true},
-
-  app: {
-    head: {
-
-    }
-  },
-
-  content: {
-      defaultLocale: 'pt-BR',
-      documentDriven: true,
-      markdown: {
-          remarkPlugins: {
-              'remark-toc': {
-                  heading: '(table[ -]of[ -])?conteúdo|sumário|contents?|toc'
-              },
-              'remark-gfm': {},
-          }
-
-      },
-      highlight: {
-          theme: 'dracula',
-          langs: [
-              'java',
-              'shell',
-              'xml',
-              'html',
-          ]
-
-      },
-  },
-
-  modules: [// make sure to add before content!
-  'nuxt-content-assets', '@nuxt/content', "@nuxt/image"],
-
-  nitro: {
-      prerender: {
-          crawlLinks: true,
-          routes: ['/atom.xml','/rss.xml','/sitemap.xml']
       }
-  },
+    },
+    content: {
+        defaultLocale: 'pt-BR',
+        documentDriven: true,
+        markdown: {
+            remarkPlugins: {
+                'remark-toc': {
+                    heading: '(table[ -]of[ -])?conteúdo|sumário|contents?|toc'
+                },
+                'remark-gfm': {},
+            }
 
-  compatibilityDate: '2024-07-08'
+        },
+        highlight: {
+            theme: 'dracula',
+            langs: [
+                'java',
+                'shell',
+                'xml',
+                'html',
+            ]
+
+        },
+    },
+    modules: [
+        'nuxt-content-assets', // make sure to add before content!
+        '@nuxt/content'
+    ],
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+            routes: ['/atom.xml','/rss.xml','/sitemap.xml']
+        }
+    }
+
 })
