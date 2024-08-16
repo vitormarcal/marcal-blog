84a19c8f9f9644082132ddce5d1317f3ec659393
Vítor Marçal <codehero@marcal.dev>
2024-08-16 17:15:10 -0300

    add IamNotARobot component

---
+++ components/IamNotARobot.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import iAmNotARobot from "assets/img/IAmNotARobot.svg";
+import madeByHuman from "assets/img/MadeByAHuman.svg";
+import writtenByHuman from "assets/img/WrittenByAHuman.svg";
+import humanContent from "assets/img/HumanContent.svg";
+</script>
+
+<template>
+  <div class="image-wrapper">
+    <img  :src="iAmNotARobot" alt="I am not a Robot" />
+    <img  :src="madeByHuman" alt="Made by human" />
+    <img  :src="writtenByHuman" alt="Written by Human" />
+    <img  :src="humanContent" alt="Human content" />
+  </div>
+</template>
+
+<style scoped>
+
+.image-wrapper {
+  display: grid;
+  grid-auto-flow: row dense;
+  padding-left: 2rem;
+  grid-template-columns: 2fr  2fr ;
+}
+
+.image-wrapper img {
+  margin-right: 1rem;
+  margin-bottom:  1rem;
+  width: 100px;
+}
+
+</style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,6 +4,7 @@
       <template v-if="showCommentArea">
         <hr/>
         <email-me-a-comment :email-address="'oi@marcal.dev'" :page-title="page.title" />
+        <iam-not-a-robot/>
       </template>
       <hr/>
     </main-content>
@@ -15,6 +16,7 @@
 </style>
 <script setup lang="ts">
 import EmailMeAComment from "~/components/EmailMeAComment.vue";
+import IamNotARobot from "~/components/IamNotARobot.vue";
 
 const { page } = useContent()
 
