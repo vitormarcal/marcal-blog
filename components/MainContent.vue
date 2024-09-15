cca0e0df6cd0885245096b64f7ab6e1d8cb50fc5
Vítor Marçal <codehero@marcal.dev>
2024-09-15 19:12:11 -0300

    mv css

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -32,55 +32,5 @@ const {page} = useContent()
 </template>
 
 <style>
-main {
-  flex-grow: 1;
-  padding: clamp(2.8rem, 2.8rem + 1.5vw, 4rem) clamp(24px, 1.032rem + 1.9355vw, 48px) 8rem
-}
 
-main div {
-  margin: 0 auto;
-  max-width: 1440px;
-}
-
-.post-section {
-  margin-top: 10px;
-
-  line-height: 1.7;
-  position: relative;
-
-  font-size: 20px;
-  letter-spacing: 0;
-  word-break: break-word;
-
-  display: grid;
-  grid-template-columns: repeat(12, minmax(0, 1fr));
-}
-
-.post-section > :is(p, ul, ol) {
-  font-weight: 480;
-}
-
-.post-section:not(.content-section)  > p:first-of-type {
-  margin-top: 2rem;
-  color: hsla(0, 0%, 100%, .64);
-  font-size: 24px;
-  font-weight: 500;
-  line-height: 1.45;
-  max-width: 840px;
-}
-
-.post-section > * {
-  grid-column: 4/span 6;
-}
-
-@media (max-width: 767px) {
-  .post-section > * {
-    grid-column: 1/span 12;
-  }
-}
-
-.post-section > * + * {
-  margin-bottom: 0;
-  margin-top: calc(1.6em * 1.5);
-}
 </style>
