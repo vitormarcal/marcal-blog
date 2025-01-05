e7a19d82fd5268fe78203d7cb87077b147da1015
Vítor Marçal <codehero@marcal.dev>
2025-01-05 18:56:55 -0300

    add book tracker 1.0

---
+++ layouts/BookLayout.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'pt-BR',
+    class: 'book-tracker'
+  },
+  bodyAttrs: {
+    class: 'book-tracker'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico'
+    }
+  ]
+})
+
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>marcal's Book Tracker</h1>
+      <p class="quote">"Os livros são sonhos que seguramos em nossas mãos."</p>
+      <img src="https://via.placeholder.com/60x60" alt="Ícone de livro" class="floating-book">
+    </header>
+    <slot />
+  </main>
+</template>
+
+<style>
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-10px);
+  }
+  60% {
+    transform: translateY(-5px);
+  }
+}
+
+body.book-tracker {
+  --blockquote-border-color: #6c4a4a;
+  --blockquote-padding-left: 0;
+
+
+  font-family: 'Merriweather', serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f7f3e9;
+  color: #333;
+
+  header {
+    background-color: #6c4a4a;
+    color: #fff;
+    padding: 40px 10px;
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: bold;
+    position: relative;
+    overflow: hidden;
+  }
+
+  header {
+    background-color: #6c4a4a;
+    color: #fff;
+    padding: 40px 10px;
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: bold;
+    position: relative;
+    overflow: hidden;
+  }
+  header h1 {
+    animation: fadeIn 1s ease-in-out;
+  }
+  header .quote {
+    margin-top: 20px;
+    font-size: 1.2rem;
+    font-style: italic;
+    animation: fadeIn 2s ease-in-out;
+  }
+  header .floating-book {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 60px;
+    animation: bounce 2s infinite;
+  }
+
+  .book-tracker-container {
+    max-width: 1000px;
+    margin: 30px auto;
+    background: #fff;
+    border-radius: 12px;
+    box-shadow: 0 6px 10px rgba(0, 0, 0, 0.1);
+    padding: 30px;
+  }
+
+}
+
+
+</style>
---
+++ pages/books/[...slug].vue
@@ -0,0 +1,186 @@
+<script setup lang="ts">
+
+import {formatDate} from "~/utils/date-utils";
+
+definePageMeta({
+  layout: 'book-layout',
+})
+const route = useRoute()
+const postName = route.params.slug[0]
+
+const post = await queryContent()
+    .where({
+      _stem: {$eq: postName},
+      tags: {$contains: 'livros'},
+      is_post: {$eq: true},
+      _partial: false
+    })
+    .findOne()
+
+
+const feedbackValue = computed<'gostei' | 'neutro' | 'não gostei' | ''>(() => post.feedback[0] || '');
+const feedback = computed(() => {
+  const feedbackMap: Record<'gostei' | 'neutro' | 'não gostei', { feedbackClass: string, text: string }> = {
+    'gostei': { feedbackClass: 'liked', text: "Gostei" },
+    'neutro': { feedbackClass: 'neutral', text: "Neutro" },
+    'não gostei': { feedbackClass: 'not-liked', text: "Não gostei" }
+  };
+  return feedbackMap[feedbackValue.value as 'gostei' | 'neutro' | 'não gostei'] || { feedbackClass: 'not-evaluated', text: "Não avaliado" };
+});
+
+</script>
+
+<template>
+  <div class="book-tracker-container">
+
+    <div class="book-header">
+      <img :src="post.image" alt="Capa do livro">
+      <div class="book-info">
+        <h1>{{ post.title }}</h1>
+        <p><strong>Autor(es):</strong> {{ post.author }}</p>
+        <p><strong>Data de leitura:</strong> {{ formatDate(post.created_at) }}</p>
+        <p><strong>Indicativo:</strong> <span :class="feedback.feedbackClass">{{ feedback.text }}</span></p>
+        <div class="details-grid">
+          <p><strong>ISBN:</strong> 978-1234567890</p>
+          <p><strong>Número de páginas:</strong> 200</p>
+          <div>
+            <p><strong>Gêneros:</strong></p>
+            <span class="badge">Ação</span>
+            <span class="badge">Aventura</span>
+            <span class="badge">Clássico</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="review-section">
+      <h2>Minha Review</h2>
+      <ContentDoc :path="post._path" v-slot="{ doc }">
+        <ContentRenderer :value="doc" />
+      </ContentDoc>
+    </div>
+
+    <div class="author-section" >
+      <h2>Sobre o Autor</h2>
+      <p>Buronson é um renomado escritor de mangás, conhecido por suas histórias cativantes e impactantes. Tetsuo Hara, co-autor, é um dos ilustradores mais icônicos do Japão, famoso por seu traço detalhado e expressivo.</p>
+    </div>
+    <div class="related-section">
+      <h2>Outros Livros Relacionados</h2>
+      <div class="related-books">
+        <img src="https://via.placeholder.com/120x180" alt="Livro relacionado 1">
+        <img src="https://via.placeholder.com/120x180" alt="Livro relacionado 2">
+        <img src="https://via.placeholder.com/120x180" alt="Livro relacionado 3">
+      </div>
+    </div>
+
+
+    <NuxtLink to="/books" class="back-link">Ver lista de livros lidos</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.book-tracker-container {
+  .book-header {
+    display: flex;
+    gap: 20px;
+    align-items: flex-start;
+  }
+  .book-header img {
+    width: 180px;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+  .book-info {
+    flex: 1;
+  }
+  .book-info h1 {
+    margin: 0 0 10px;
+    font-size: 1.8rem;
+    color: #6c4a4a;
+  }
+  .book-info p {
+    margin: 5px 0;
+    font-size: 1rem;
+    line-height: 1.6;
+  }
+  .details-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+    margin: 20px 0;
+  }
+  .details-grid p {
+    margin: 0;
+    font-size: 1rem;
+  }
+  .badge {
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 4px;
+    font-size: 0.9rem;
+    margin: 2px;
+    background-color: #f0e6d2;
+    color: #6c4a4a;
+  }
+  .review-section {
+    margin-top: 30px;
+  }
+  .review-section h2 {
+    color: #6c4a4a;
+    margin-bottom: 10px;
+  }
+  .review-section:deep() p {
+    font-size: 1rem;
+    line-height: 1.6;
+    background-color: #f9f8f5;
+    padding: 15px;
+    margin: 1rem 0;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  .related-section, .author-section {
+    margin-top: 30px;
+  }
+  .related-section h2, .author-section h2 {
+    margin-bottom: 10px;
+    color: #6c4a4a;
+  }
+  .related-books {
+    display: flex;
+    gap: 20px;
+    overflow-x: auto;
+  }
+  .related-books img {
+    width: 120px;
+    height: auto;
+    border-radius: 8px;
+    transition: transform 0.3s ease;
+  }
+  .related-books img:hover {
+    transform: scale(1.1);
+  }
+  .author-section p {
+    font-size: 1rem;
+    line-height: 1.6;
+  }
+  .back-link {
+    display: inline-block;
+    margin-top: 20px;
+    text-decoration: none;
+    color: #6c4a4a;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+  .back-link:hover {
+    text-decoration: underline;
+  }
+}
+
+/* to hide feature image from markdown files (when img is the first content) */
+.review-section:deep() div p:first-of-type:has(img) {
+  display: none ;
+}
+
+</style>
